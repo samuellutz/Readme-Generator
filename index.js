00e2c3b69b8d7cb648490e85592972dfac94cd59
samuel lutz <samlutz89@gmail.com>
2022-01-20 18:58:35 -0800

    got input to appear

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generatePage = require("./utils/generateMarkdown");
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    // using inquirer to prompt questions to user 
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false; 
+            }
+        } 
+    }]);
+}
+questions()
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
